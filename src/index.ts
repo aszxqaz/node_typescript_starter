97de519fa4b5f5dbeb72f94a6a4253610bc338c7
Maxim B <b.maxim.dev@gmail.com>
2023-05-05 14:58:53 +0400

    asdas

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import os from 'os';
 
 console.log(process.env.PORT)
-console.log(JSON.stringify(os, null, 2));
+console.log(`Arch: ${os.arch()}`);
+console.log(`Cpus: ${JSON.stringify(os.cpus(), null, 2)}`);
+console.log(`Total memory: ${os.totalmem()}`);
+console.log(`Free memory in mb: ${os.freemem() / 1024}`);
+console.log(`Platform: ${os.platform()}`);
+console.log(`Available parallelism: ${os.availableParallelism()}`);
+console.log(`Machine: ${os.machine()}`);
+console.log(`OS: ${os.type()}`);
+console.log(`Version: ${os.version()}`);
+console.log(`Release: ${os.release()}`);
